31cb9d1eacde9caf867839092d66a9b492d54405
Vincent Jousse <vincent@jousse.org>
2017-09-03 22:17:29 +0200

    Display icons for a specific service only

--- convert.py
+++ convert.py
@@ -9,7 +9,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def create_list():
+def create_list(
+        emojione,
+        google,
+        apple,
+        facebook,
+        twitter,
+        messenger):
 
     sorted_short_names = collections.OrderedDict(
         sorted(emoji_data_python.emoji_short_names.items()))
@@ -20,24 +26,30 @@ def create_list():
     elm_list = "[ "
     for short_name, values in sorted_short_names.items():
         dict_values = values.__dict__
+
         logger.debug("{} {}".format(
             dict_values['short_name'],
             dict_values['unified']))
 
-        if first:
+        if (emojione and dict_values['has_img_emojione']) or \
+                (google and dict_values['has_img_google']) or \
+                (apple and dict_values['has_img_apple']) or \
+                (facebook and dict_values['has_img_facebook']) or \
+                (twitter and dict_values['has_img_twitter']) or \
+                (messenger and dict_values['has_img_messenger']):
+
+            if not first:
+                elm_list = elm_list + '\n, '
+            else:
+                first = False
+
             elm_list = \
                 elm_list + '( "{}", [ "{}" ] )'.format(
                     dict_values['short_name'],
                     dict_values['unified'].lower())
 
-            first = False
-        else:
-            elm_list = \
-                elm_list + '\n, ( "{}", [ "{}" ] )'.format(
-                    dict_values['short_name'],
-                    dict_values['unified'].lower())
-
     elm_list = elm_list + "\n]"
+
     return elm_list
 
 
@@ -51,9 +63,46 @@ if __name__ == '__main__':
             help="Increase output verbosity",
             action="store_true")
 
+    parser.add_argument(
+            "--emojione",
+            help="Add emojione icons",
+            action="store_true")
+
+    parser.add_argument(
+            "--google",
+            help="Add google icons",
+            action="store_true")
+
+    parser.add_argument(
+            "--apple",
+            help="Add apple icons",
+            action="store_true")
+
+    parser.add_argument(
+            "--facebook",
+            help="Add facebook icons",
+            action="store_true")
+
+    parser.add_argument(
+            "--twitter",
+            help="Add twitter icons",
+            action="store_true")
+
+    parser.add_argument(
+            "--messenger",
+            help="Add messenger icons",
+            action="store_true")
+
     args = parser.parse_args()
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    print(create_list())
+    print(create_list(
+        args.emojione,
+        args.google,
+        args.apple,
+        args.facebook,
+        args.twitter,
+        args.messenger
+        ))
